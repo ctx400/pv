e77d8ff84050a136226d786cac2546db2101ef62
ctx400 <43049420+ctx400@users.noreply.github.com>
2025-05-17 00:07:44 -0500

    Made __init__.py more readable.
    I mean, as readable as multi-decorator littered functions can be :)

--- src/pv/__init__.py
+++ src/pv/__init__.py
@@ -12,45 +12,64 @@ from .vault import Argon2idKDF as Argon2idKDF
 from .vault import new_salt as new_salt
 
 
+#
+# kwargs for various arguments
+#
+common_args = { #type:ignore
+    'path_type': Path,
+    'allow_dash': False,
+    'dir_okay': False,
+    'file_okay': True,
+    'executable': False,
+    'resolve_path': True,
+}
+create_args = { #type:ignore
+    **common_args,
+    'exists': False,
+    'writable': True,
+    'readable': False,
+}
+store_args = { #type:ignore
+    **common_args,
+    'exists': True,
+    'writable': True,
+    'readable': True,
+}
+read_args = { #type:ignore
+    **common_args,
+    'exists': True,
+    'writable': False,
+    'readable': True,
+}
+
+
+# Dummy function as the root command
 @click.group()
 def pv() -> None:
     pass
 
+
+# Create a new PV vault.
 @pv.command('create')
-@click.argument('path',
-                required=True,
-                type=click.Path(
-                    path_type=Path,
-                    allow_dash=False,
-                    exists=False,
-                    dir_okay=False,
-                    file_okay=True,
-                    resolve_path=True,
-                    writable=True,
-                    readable=False,
-                    executable=False,
-                ))
+@click.argument(
+        'path',
+        required=True,
+        type=click.Path(**create_args)) #type:ignore
 def create_vault(path: Path) -> None:
     pv = PV()
     pv.save(path)
 
+
+# Store a secret in the vault.
 @pv.command('store')
-@click.argument('key',
-                required=True,
-                type=click.STRING)
-@click.argument('path',
-                required=True,
-                type=click.Path(
-                    path_type=Path,
-                    allow_dash=False,
-                    exists=True,
-                    dir_okay=False,
-                    file_okay=True,
-                    resolve_path=True,
-                    writable=True,
-                    readable=True,
-                    executable=False,
-                ))
+@click.argument(
+        'key',
+        required=True,
+        type=click.STRING)
+@click.argument(
+        'path',
+        required=True,
+        type=click.Path(**store_args)) #type:ignore
 def store_secret(key: str, path: Path) -> None:
     secret: str = getpass('Secret Value: ')
     password: bytes = getpass('Master Password: ').encode()
@@ -58,64 +77,45 @@ def store_secret(key: str, path: Path) -> None:
     pv.store_secret(key, secret, password)
     pv.save(path)
 
+
+# Read a secret from the vault.
 @pv.command('read')
-@click.argument('key',
-                required=True,
-                type=click.STRING)
-@click.argument('path',
-                required=True,
-                type=click.Path(
-                    path_type=Path,
-                    allow_dash=False,
-                    exists=True,
-                    dir_okay=False,
-                    file_okay=True,
-                    resolve_path=True,
-                    writable=False,
-                    readable=True,
-                    executable=False,
-                ))
+@click.argument(
+        'key',
+        required=True,
+        type=click.STRING)
+@click.argument(
+        'path',
+        required=True,
+        type=click.Path(**read_args)) #type:ignore
 def read_secret(key: str, path: Path) -> None:
     password: bytes = getpass('Master Password: ').encode()
     pv = PV.load(path)
     print(pv.read_secret(key, password))
 
+
+# Delete a secret from the vault.
 @pv.command('delete')
-@click.argument('key',
-                required=True,
-                type=click.STRING)
-@click.argument('path',
-                required=True,
-                type=click.Path(
-                    path_type=Path,
-                    allow_dash=False,
-                    exists=True,
-                    dir_okay=False,
-                    file_okay=True,
-                    resolve_path=True,
-                    writable=True,
-                    readable=True,
-                    executable=False,
-                ))
+@click.argument(
+        'key',
+        required=True,
+        type=click.STRING)
+@click.argument(
+        'path',
+        required=True,
+        type=click.Path(**store_args)) #type:ignore
 def delete_secret(key: str, path: Path) -> None:
     pv = PV.load(path)
     pv.delete_secret(key)
     pv.save(path)
 
+
+# List all secrets in the vault.
 @pv.command('list')
-@click.argument('path',
-                required=True,
-                type=click.Path(
-                    path_type=Path,
-                    allow_dash=False,
-                    exists=True,
-                    dir_okay=False,
-                    file_okay=True,
-                    resolve_path=True,
-                    writable=False,
-                    readable=True,
-                    executable=False,
-                ))
+@click.argument(
+        'path',
+        required=True,
+        type=click.Path(**read_args)) #type:ignore
 def list_secrets(path: Path) -> None:
     pv = PV.load(path)
     [print(key) for key in pv.list_secrets()]
