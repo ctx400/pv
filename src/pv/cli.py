4693464bf6df64b18dc0ad3eea7a8ca65af55a9c
ctx400 <43049420+ctx400@users.noreply.github.com>
2025-05-21 21:57:55 -0500

    CLI: updated docstring

--- src/pv/cli.py
+++ src/pv/cli.py
@@ -8,16 +8,25 @@ the PV secrets vault. The module is a thin wrapper over PV's API.
 Create a new vault:
 
 ```console
-user@host:~$ pv create pv.json
+user@host:~$ pv create --path pv.json
+Master Password: ********
+Confirm Password: ********
+
 user@host:~$
 ```
 
 Store a secret in the vault:
 
+> **Tip:** As a shortcut, you can set the `PV_PATH` environment variable
+  to avoid having to pass `--path` to every command. If you're brave
+  enough, you can also set `PV_PASSWORD` to avoid typing your master
+  password for every `store` and `read` operation.
+
 ```console
-user@host:~$ pv store mykey pv.json
+user@host:~$ export PV_PATH='pv.json'
+user@host:~$ export PV_PASSWORD='my-master-password'
+user@host:~$ pv store mykey
 Secret Value: ********
-Master Password: ********
 
 user@host:~$
 ```
@@ -25,7 +34,7 @@ user@host:~$
 List all secrets in a vault:
 
 ```console
-user@host:~$ pv list pv.json
+user@host:~$ pv list
 secret1
 hello-world
 google
@@ -38,8 +47,7 @@ user@host:~$
 Read a secret from the vault:
 
 ```console
-user@host:~$ pv read mykey pv.json
-Master Password: ********
+user@host:~$ pv read mykey
 my secret value
 
 user@host:~$
@@ -48,7 +56,14 @@ user@host:~$
 Delete a secret from the vault:
 
 ```console
-user@host:~$ pv delete mykey pv.json
+user@host:~$ pv delete mykey
+user@host:~$
+```
+
+To get help on any command, just run:
+
+```console
+user@host:~$ pv COMMAND --help
 user@host:~$
 ```
 '''
